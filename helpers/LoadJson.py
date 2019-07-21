51881907b8206fc97c697f4e9e92f4b54b122d67
Alon Weinstein <alon.v1@gmail.com>
2019-07-21 06:31:48 +0300

    cat_to_name

--- helpers/LoadJson.py
+++ helpers/LoadJson.py
@@ -1,5 +1,7 @@
 # LABEL MAPPING
 import json
 
-with open('cat_to_name.json', 'r') as f:
-    cat_to_name = json.load(f)
+def load_cat_to_names_json(cat_to_name_path):
+    with open(cat_to_name_path, 'r') as f:
+        cat_to_name = json.load(f)
+    return cat_to_name
